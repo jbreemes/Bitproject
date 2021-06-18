d65072af78cc34e1045d90c552c1fcb7e0b326c8
Mehran <mshahbackhsh@roc-dev.com>
2021-06-18 10:36:15 +0200

    Update DBconn.php
    new codes to limit the spam in db

--- Inlogpagina/DBconn.php
+++ Inlogpagina/DBconn.php
@@ -11,7 +11,28 @@ $db = new mysqli($servername, $username, $password, $database);
 if ($db->connect_error) {
   die("Connection failed: " . $db->connect_error);
 }
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $stmt = $conn->prepare("Select last_time from test.lastcheck order by last_time asc limit 1"); 
+  $stmt->execute();
 
+  // set the resulting array to associative
+  $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+  foreach($stmt->fetchAll() as $k=>$v) { 
+      $last = $v;
+  }
+}
+catch(PDOException $e) {
+  $conn = null;
+}
+$conn = null;
+
+
+echo '<form id=theForm>
+   <input type=hidden value="'.$last.'" name=last id=last></input>
+   <input type=button onclick="submitFunction()"></input>
+   </form>';
 
 // $sql = "SELECT * FROM `student`";
 // $result = $db->query($sql);
