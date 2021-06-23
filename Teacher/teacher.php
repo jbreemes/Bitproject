c22967aa217087dd7a6eb46fb215e657defe197e
machbi <55505637+machbi@users.noreply.github.com>
2021-06-23 22:51:04 +0200

    All students hidden on landing page by default, breaks some of the CSS.

--- Teacher/teacher.php
+++ Teacher/teacher.php
@@ -75,7 +75,8 @@ if ($result ->num_rows > 0) {
                   border-collapse: collapse;
                 }
                 </style>
-                  <div id = "table_slot">
+
+                  <div id = "table_slot" style="display: none">
                     <table>
                         <th style="width: 10%">ID</th>
                         <th>First name</th>
@@ -99,6 +100,7 @@ if ($result ->num_rows > 0) {
        // Action listener for button 1
        document.getElementById("classBtn1").addEventListener("click", function()
        {
+      // document.getElementById("table_slot").style.display = "block";
        document.getElementById("table1").style.display = "block";
        document.getElementById("table2").style.display = "none";
        document.getElementById("table3").style.display = "none";
