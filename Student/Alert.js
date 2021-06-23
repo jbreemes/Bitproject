95e24fd347210220ad3ec67592b5139ac3f3927f
Serferaaz <54798372+Serferaaz@users.noreply.github.com>
2021-06-23 14:33:35 +0200

    made an alert and put requirements for the buttons and textarea

--- Student/Alert.js
+++ Student/Alert.js
@@ -1,5 +1,5 @@
-// var alertMe = document.querySelector("#LetUsKnowButton");
+var alertMe = document.querySelector("#LetUsKnowButton");
 
-// alertMe.addEventListener('click', function(){
-// window.alert("Thank you for filling the form, you can now log out!");
-// });
+alertMe.addEventListener('click', function(){
+window.alert("Thank you for filling the form, you can now log out!");
+});
